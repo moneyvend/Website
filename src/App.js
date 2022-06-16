92f83afecfc59c28cbeffa1d2f9f9f3c6b3a9e45
isaacpitwa <isaacpitwa256@gmail.com>
2022-06-16 21:18:23 +0300

    [Fix] linter errors + folder structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Beneficiaries from './components/pages/beneficiaries';
-import ComingSoon from './components/pages/coming-soon/comingSoon';
-import PaymentServices from './components/pages/paymentServices';
-import Settings from './components/pages/settings';
-import Transaction from './components/pages/Transaction';
+import Beneficiaries from './components/beneficiaries/beneficiaries';
+import ComingSoon from './components/shared/coming-soon/comingSoon';
+import PaymentServices from './components/payments/paymentServices';
+import Settings from './components/settings/settings';
+import Transaction from './components/transactions/transaction';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ComingSoon />} />
-      <Route path="Settings" element={<Settings />} />
-      <Route path="Transactions" element={<Transaction />} />
-      <Route path="Beneficiaries" element={<Beneficiaries />} />
-      <Route path="PaymentServices" element={<PaymentServices />} />
+      <Route path="dashboard">
+        <Route element={<>This is the Home for dashboard</>} index />
+        <Route path="settings" element={<Settings />} />
+        <Route path="transactions" element={<Transaction />} />
+        <Route path="beneficiaries" element={<Beneficiaries />} />
+        <Route path="payments" element={<PaymentServices />} />
+      </Route>
     </Routes>
 
   );
