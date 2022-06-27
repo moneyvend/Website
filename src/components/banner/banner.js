5b24e53e6710c6877e2600a9704d8f79ec1fee86
buhariaminat <buhariaminat@yahoo.com>
2022-06-27 20:54:29 +0100

    [ADD] images

---
+++ src/components/banner/banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const Banner = () => (
+  <Layout>
+    <div>
+      <ul>
+        <p>
+          payment services
+        </p>
+        <h1>
+          The simplest way for fast payments
+        </h1>
+        <button type="button">Bill payments</button>
+        <button type="button">Become a merchant</button>
+      </ul>
+      <img src="/" alt="image1" />
+    </div>
+  </Layout>
+);
+
+export default Banner;
--- src/utilities/images/images.js
+++ src/utilities/images/images.js
@@ -1,4 +1,16 @@
 class AppImages {
 static LOGO_VERT = `${process.env.PUBLIC_URL}/assets/logo/logo-vert.svg`;
+
+static IEDC = `${process.env.PUBLIC_URL}/assets/logo/iedc.svg`;
+
+static DSTV = `${process.env.PUBLIC_URL}/assets/logo/dstv.svg`;
+
+static IKEJA_LOGO = `${process.env.PUBLIC_URL}/assets/logo/ikejaelectric.svg`;
+
+static UPAY = `${process.env.PUBLIC_URL}/assets/logo/upay.svg`;
+
+static MTN = `${process.env.PUBLIC_URL}/assets/logo/mtn.svg`;
+
+static GOTV = `${process.env.PUBLIC_URL}/assets/logo/gotv.svg`;
 }
 export default AppImages;
