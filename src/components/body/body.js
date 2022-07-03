ddd9dd38e37ad19d1f1992288ec62876a70cbce7
buhariaminat <buhariaminat@yahoo.com>
2022-07-03 22:42:28 +0100

    Add svg images from Figma

---
+++ src/components/body/body.js
@@ -0,0 +1,37 @@
+import { Layout } from 'antd';
+
+const body = () => (
+  <Layout>
+    <h2>Why monievend ?</p>
+    <div>
+      <div>
+        <h3>Easy, Simple, Quick and Safe</h3>
+        <p>
+          All customer transactions get the special treatment. 
+          Cutting edge technology for super fast,
+          super safe and an easy to use interface for the best possible experience working with us
+
+        </p>
+      </div>
+      <div>
+        <h3>Guaranteed Fluidity</h3>
+        <p>
+          With cutting edge and industry leading software and API’s integrated into the system,
+          you can always expect the best of the best services always
+
+        </p>
+      </div>
+    </div>
+    <h2>Our Services</h2>
+    <div>
+        <div>
+            <h4>Utility Bill Payment</h4>
+            <p>Pay for your DSTV, GoTV Startimes and more with our integrated</p>
+        </div>
+    </div>
+
+
+  </Layout>
+);
+
+export default body;
