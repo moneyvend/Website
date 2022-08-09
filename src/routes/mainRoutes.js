841edd53d867d3716c71dac396b437e756e8f337
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-08-09 23:40:11 +0300

    Add Not Found page

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,6 +4,7 @@ import Banner from '../components/banner/banner';
 import Body from '../components/body/body';
 import NavBar from '../components/navigation/navBar';
 import ComingSoon from '../components/shared/coming-soon/comingSoon';
+import NotFound from '../components/shared/not-found/notFound';
 
 class MainRoutes {
     static routes =
@@ -18,6 +19,7 @@ class MainRoutes {
         <Route path="merchants" element={<ComingSoon/>} />,
         <Route path="about" element={<ComingSoon/>} />,
         <Route path="support" element={<ComingSoon/>} />,
+        <Route path="*" element={<NotFound/>} />,
     ]
 }
 export default MainRoutes;
