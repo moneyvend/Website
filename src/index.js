7105c80e96684b85990127175b2301fc8ea5115f
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-08-09 17:01:57 +0300

    Polish  header nav styles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import App from './App';
-import ComingSoon from './components/shared/coming-soon/comingSoon';
-import Transaction from './components/transactions/transaction';
-import Banner from './components/banner/banner';
-import Body from './components/body/body';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,9 +9,6 @@ root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-      {/* <ComingSoon /> */}
-      {/* <Banner /> */}
-      {/* <Transaction /> */}
     </BrowserRouter>
   </React.StrictMode>,
 );
