68cb32d48e90462578236595b99de4e9d162c52f
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-12-06 18:27:02 +0100

    update

---
+++ src/components/users-page/payment-services/payment-services.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+import React from 'react';
+
+export default function PaymentService() {
+    return (
+        <section>
+            <div>
+                working well
+            </div>
+        </section>
+    );
+}
--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,55 +1,14 @@
 /* eslint-disable react/jsx-indent */
 import { Route } from 'react-router-dom';
 import Index from '../components/users-page';
-import Beneficiaries from '../components/beneficiaries/beneficiaries';
-import PaymentServices from '../components/payments/paymentServices';
-import Settings from '../components/settings/settings';
-import Transaction from '../components/transactions/transaction';
-import Services from '../components/payments/services';
-// import Airtime from '../components/payments/Airtime';
-// import Electricity from '../components/payments/electricity';
-// import Summary from '../components/payments/summary';
-// import Method from '../components/payments/method';
-// import Recepient from '../components/payments/recepient';
-// import Notransaction from '../components/transactions/noTransaction';
+import PaymentService from '../components/users-page/payment-services/payment-services';
 
 class DashboardRoutes {
   static routes =
-
     <Route path="dashboard" element={<Index />}>
       <Route path="services">
-        <Route index element={<Services />} />
-      </Route>
-      <Route path="payments">
-        <Route index element={<PaymentServices />} />
-
-      </Route>
-
-      <Route path="transactions">
-        <Route index element={<Transaction />} />
-      </Route>
-
-      <Route path="beneficiaries">
-        <Route index element={<Beneficiaries />} />
-      </Route>
-
-      {/* <Route path="notification">
-        <Route index={true} element={<Notification />} />
-      </Route> */}
-
-      <Route path="settings">
-        <Route index element={<Settings />} />
+        <Route index element={<PaymentService />} />
       </Route>
     </Route>
-
-  // <Route path="noTransactions" element={<Notransaction />} />
-  // <Route path="beneficiaries" element={<Beneficiaries />} />
-  // <Route path="payments" element={<PaymentServices />} />
-
-  // <Route path="payments/airtime" element={<Airtime />} />
-  // <Route path="payments/electricity" element={<Electricity />} />
-  // <Route path="payments/summary" element={<Summary />} />
-  // <Route path="payments/method" element={<Method />} />
-  // <Route path="payments/recepient" element={<Recepient />} />
 }
 export default DashboardRoutes;
