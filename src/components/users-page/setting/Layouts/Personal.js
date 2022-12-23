7f3139fdf3c3037dfe6a74fdc849b162e08bdd16
Ayeni Emmanuel Olakunle <ayeniolakunle98@gmail.com>
2022-12-23 13:20:33 +0100

    update

---
+++ src/components/users-page/setting/Layouts/Personal.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PersonalInfo from './Personal.module.scss';
+
+function Personal(props) {
+    return (
+        <section>
+            <div>
+                <h1>Personal Information</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Personal;
