a2a1cc4e723c602325bea432d54620ab25e131f4
Ayeni Emmanuel Olakunle <ayeniolakunle98@gmail.com>
2022-12-23 14:19:31 +0100

    update

--- src/components/users-page/setting/Layouts/Personal.js
+++ src/components/users-page/setting/Layouts/Personal.js
@@ -2,13 +2,55 @@
 /* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import {
+    Form,
+    Col,
+    Row,
+    Button,
+} from 'react-bootstrap';
 import PersonalInfo from './Personal.module.scss';
 
 function Personal(props) {
     return (
         <section>
-            <div>
-                <h1>Personal Information</h1>
+            <div className={PersonalInfo.holdAll}>
+                <div className={PersonalInfo.holdAlls}>
+                    <div className={PersonalInfo.holdImage}>
+                        <input type="file" accept="image/png" alt="Profile" />
+                    </div>
+                </div>
+                <div className={PersonalInfo.holdForm}>
+                    <Form>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridFirstName">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="text" placeholder="First Name" value="Diamond" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridFirstName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="text" placeholder="Last Name" value="Kekule" />
+                            </Form.Group>
+                        </Row>
+
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" value="Diamondkekule98@gmail.com" />
+                            </Form.Group>
+
+                            <Form.Group as={Col} controlId="formGridPhone">
+                                <Form.Label>Phone</Form.Label>
+                                <Form.Control type="Phone" placeholder="Phone" value="09098402605" />
+                            </Form.Group>
+                        </Row>
+                        <div className={PersonalInfo.holdButin}>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
             </div>
         </section>
     );
