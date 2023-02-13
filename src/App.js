e4bd3b5e1341353dc470b6f358d26ee21688f978
Naggenda <naggendajoshua@gmail.com>
2023-02-13 21:55:05 +0900

    changing app.js file to initial stage:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// import {
-// Routes,
-// } from 'react-router-dom';
-// import AppRoutes from './routes';
-import Login from './pages/auth/login';
+import {
+  Routes,
+} from 'react-router-dom';
+import AppRoutes from './routes';
 
 const App = () => (
-  <Login />
-  // {<Routes>}
-  // {  {AppRoutes.map((route) => route)}}
-  // {</Routes>}
+  <Routes>
+    {AppRoutes.map((route) => route)}
+  </Routes>
 );
 
 export default App;
