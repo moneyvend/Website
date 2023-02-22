5289801c688b9b0bb3bd85f6911a8f5c600ddce1
Naggenda <naggendajoshua@gmail.com>
2023-02-23 05:02:16 +0900

    cable tv layout

--- src/components/users-page/payment-services/Layout/Cabletv.js
+++ src/components/users-page/payment-services/Layout/Cabletv.js
@@ -13,23 +13,47 @@ import {
 import { useNavigate } from 'react-router-dom';
 import electrictyStyle from './Electricity.module.scss';
 
+// "smartcard_number": "1020833447",
+// "total_amount": 5300,
+// "product_monthsPaidFor": 1,
+// "service_type": "dstv",
+// "phone": "08189392459",
+
 function Electricity(props) {
+    const [service, setService] = useState('');
+    const [amount, setAmount] = useState('');
+    const [month, setMonth] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [phone, setPhone] = useState('');
+
+    const handleSubmit = async () => {
+
+    };
+
     return (
         <section>
             <div className={electrictyStyle.holdAll}>
                 <div className={electrictyStyle.hold2F}>
                     <h3 style={{ marginBottom: '20px' }}>Cable Tv Biller</h3>
                     <div>
-                        <Form>
+                        <Form onSubmit={handleSubmit}>
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridEmail">
-                                    <Form.Label>Account type</Form.Label>
-                                    <Form.Control type="text" placeholder="Account type" />
+                                    <Form.Label>Services</Form.Label>
+                                    <Form.Select aria-label="Default select example" className="mb-3" onChange={(e) => setService(e.target.value)}>
+                                        <option value="Dstv">Dstv</option>
+                                        <option value="Gotv">Gotv</option>
+                                        <option value="Startimes">Startimes</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridEmail">
+                                    <Form.Label>Card Number</Form.Label>
+                                    <Form.Control type="text" placeholder="Smart Card Number" />
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridPassword">
-                                    <Form.Label>Meter No</Form.Label>
-                                    <Form.Control type="password" placeholder="Meter No" />
+                                    <Form.Label>Number of Months Paid for</Form.Label>
+                                    <Form.Control type="number" placeholder="Months" />
                                 </Form.Group>
                             </Row>
                             <Row className="mb-3">
