b7e51c6f8c9b65202f11acc801760f7a95b94a21
Naggenda <naggendajoshua@gmail.com>
2023-03-22 21:04:41 +0900

    login

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -46,20 +46,11 @@ export default function RegisterPage() {
       const accessToken = response?.data?.accessToken;
       const roles = response?.data?.roles;
       setAuth({ email, password, roles, accessToken });
-      navigate('/dashboard');
       setEmail('');
       setPassword('');
+      navigate('/dashboard')
     } catch (err) {
-      if (!err?.response) {
-        setErrMsg('No Server Response');
-      } else if (err.response?.status === 400) {
-        setErrMsg('Missing Username or Password');
-      } else if (err.response?.status === 401) {
-        setErrMsg('Unauthorized');
-      } else {
-        setErrMsg('Login Failed');
-      }
-      errRef.current?.focus();
+      alert('wrong user credentials');
     }
   }
 
