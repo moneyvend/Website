7e528e074919879c357cca23262a90b0b07a010d
Naggenda <naggendajoshua@gmail.com>
2023-03-31 01:27:51 +0900

    login api reconfigure

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -8,16 +8,16 @@ import loginStyle from './login.module.scss';
 import AppImages from '../../utilities/images/images';
 
 async function LoginUser(credentials) {
-  return fetch('http://localhost:8080/login',{
-      method: 'POST',
-      header: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
+  return fetch('http://localhost:8080/login', {
+    method: 'POST',
+    header: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
   }).then(data => data.json())
 }
 
-export default function RegisterPage({setToken}) {
+export default function RegisterPage({ setToken }) {
   /* eslint-disable */
   const [switchMe, setSwitchMe] = useState('Login with email');
 
@@ -30,8 +30,8 @@ export default function RegisterPage({setToken}) {
   const [password, setPassword] = useState('');
   const [phone, setPhone] = useState('');
   const [success, setSuccess] = useState(false);
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const deviceToken = "yt7757r6yyh";
+  const [searchParams, setSearchParams] = useSearchParams();
+  const deviceToken = searchParams.get('token');
   const navigate = useNavigate();
 
   // Email Login api integration
@@ -56,7 +56,14 @@ export default function RegisterPage({setToken}) {
         'Accept': 'application/json'
       },
       body: JSON.stringify(item)
-    })
+    }).then((response) => response.json())
+      .then(() => {
+        // result = await result.json();
+        localStorage.setItem('user-info', JSON.stringify(result));
+        navigate('/dashboard');
+      }).catch(error => {
+        alert('failed wrong credentials');
+      })
   }
 
   // const emailLogin = async (e) => {
@@ -116,118 +123,118 @@ export default function RegisterPage({setToken}) {
     setSwitchMe(index);
   };
   return (
-        <section className={loginStyle.holdAll}>
-          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-          <div className={loginStyle.holdFormNText}>
-            <div className={loginStyle.holdText}>
-              <div className={loginStyle.holdImage}>
-                <img src={AppImages.LOGO_VERT} alt="Logo" />
-              </div>
-              <div>
-                <h3>Creating payment solutions</h3>
-                <p>A product which specializes in creating terminal solution products for customers, SME&apos;s and merchants. </p>
-              </div>
-            </div>
-            <div className={loginStyle.holdForm}>
-              <div className={loginStyle.holdImage}>
-                <img src={AppImages.LOGO_VERT} alt="Logo" />
-              </div>
-              <div className={loginStyle.headerText}>
-                <h3>Welcome Back!</h3>
-                <p>Please sign in to your personal account to start access all payment services.</p>
-              </div>
-              <div className={loginStyle.holdSwichButin}>
-                <button
-                  type="submit"
-                  className={switchMe === 'Login with email' ? loginStyle.acti : loginStyle.actiNot}
-                  onClick={() => {
-                    handleToggle('Login with email');
-                  }}
-                >
-                  Login with email
-                </button>
-                <button
-                  type="submit"
-                  className={switchMe === 'Login with phone number' ? loginStyle.acti : loginStyle.actiNot}
-                  onClick={() => {
-                    handleToggle('Login with phone number');
-                  }}
-                >
-                  Login with phone number
-                </button>
-              </div>
-              {switchMe === 'Login with email'
-                ? (
-                  <Form onSubmit={emailLogin}>
-                    <p className={loginStyle.inSwi}>{switchMe}</p>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control
-                        value={email}
-                        type="email"
-                        placeholder="Enter email"
-                        name="email"
-                        autoComplete='off'
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </Form.Group>
-
-                    <Form.Group className="mb-2" controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        value={password}
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                      />
-                    </Form.Group>
-
-                    <div className={loginStyle.holdRemember}>
-                      <Link to="/auth/forgot-password">Forgot Password?</Link>
-                    </div>
-
-                    <p className={loginStyle.already}>
-                      Don&apos;t have an account?
-                      <Link to="/auth/register">Sign up</Link>
-                    </p>
-
-                    <Button variant="primary" type="submit">
-                      Submit
-                    </Button>
-                  </Form>
-                )
-                : (
-                  <Form onSubmit={handleSubmit}>
-                    <p className={loginStyle.inSwi}>{switchMe}</p>
-                    <Form.Group className="mb-3">
-                      <Form.Label>Phone number</Form.Label>
-                      <Form.Control type="phone" placeholder="Phone number" name="Phone number" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                    </Form.Group>
-
-                    <Form.Group className="mb-2">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" value={password} placeholder="Password" name="password" onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-
-                    <div className={loginStyle.holdRemember}>
-                      <Link to="/auth/forgot-password">Forgot Password?</Link>
-                    </div>
-
-                    <p className={loginStyle.already}>
-                      Don&apos;t have an account?
-                      <Link to="/auth/register">Sign up</Link>
-                    </p>
-
-                    <Button variant="primary" type="submit">
-                      Submit
-                    </Button>
-                  </Form>
-                )}
-            </div>
+    <section className={loginStyle.holdAll}>
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      <div className={loginStyle.holdFormNText}>
+        <div className={loginStyle.holdText}>
+          <div className={loginStyle.holdImage}>
+            <img src={AppImages.LOGO_VERT} alt="Logo" />
           </div>
-          {/* {isLoading ? <Loader /> : null} */}
-        </section>
+          <div>
+            <h3>Creating payment solutions</h3>
+            <p>A product which specializes in creating terminal solution products for customers, SME&apos;s and merchants. </p>
+          </div>
+        </div>
+        <div className={loginStyle.holdForm}>
+          <div className={loginStyle.holdImage}>
+            <img src={AppImages.LOGO_VERT} alt="Logo" />
+          </div>
+          <div className={loginStyle.headerText}>
+            <h3>Welcome Back!</h3>
+            <p>Please sign in to your personal account to start access all payment services.</p>
+          </div>
+          <div className={loginStyle.holdSwichButin}>
+            <button
+              type="submit"
+              className={switchMe === 'Login with email' ? loginStyle.acti : loginStyle.actiNot}
+              onClick={() => {
+                handleToggle('Login with email');
+              }}
+            >
+              Login with email
+            </button>
+            <button
+              type="submit"
+              className={switchMe === 'Login with phone number' ? loginStyle.acti : loginStyle.actiNot}
+              onClick={() => {
+                handleToggle('Login with phone number');
+              }}
+            >
+              Login with phone number
+            </button>
+          </div>
+          {switchMe === 'Login with email'
+            ? (
+              <Form onSubmit={emailLogin}>
+                <p className={loginStyle.inSwi}>{switchMe}</p>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    value={email}
+                    type="email"
+                    placeholder="Enter email"
+                    name="email"
+                    autoComplete='off'
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-2" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+
+                <div className={loginStyle.holdRemember}>
+                  <Link to="/auth/forgot-password">Forgot Password?</Link>
+                </div>
+
+                <p className={loginStyle.already}>
+                  Don&apos;t have an account?
+                  <Link to="/auth/register">Sign up</Link>
+                </p>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            )
+            : (
+              <Form onSubmit={handleSubmit}>
+                <p className={loginStyle.inSwi}>{switchMe}</p>
+                <Form.Group className="mb-3">
+                  <Form.Label>Phone number</Form.Label>
+                  <Form.Control type="phone" placeholder="Phone number" name="Phone number" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-2">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" value={password} placeholder="Password" name="password" onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+
+                <div className={loginStyle.holdRemember}>
+                  <Link to="/auth/forgot-password">Forgot Password?</Link>
+                </div>
+
+                <p className={loginStyle.already}>
+                  Don&apos;t have an account?
+                  <Link to="/auth/register">Sign up</Link>
+                </p>
+
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            )}
+        </div>
+      </div>
+      {/* {isLoading ? <Loader /> : null} */}
+    </section>
   );
 }
